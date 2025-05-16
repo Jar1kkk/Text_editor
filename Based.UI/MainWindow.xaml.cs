c4c1cd75e8e3aae7a52b2ef88a8bc82a0eeed015
zdkazuha <aartemchelyk@gmail.com>
2025-05-16 20:29:17 +0300

    Update paragraph and buffer

--- Based.UI/MainWindow.xaml.cs
+++ Based.UI/MainWindow.xaml.cs
@@ -9,11 +9,18 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Based.UI
 {
     public partial class MainWindow : Window
     {
+        private int Number = 1;
+        private bool IsNotNumbered = false;
+        private bool IsNumbered = false;
+
+        private string Text = "";
         public MainWindow()
         {
             InitializeComponent();
@@ -96,25 +103,75 @@ namespace Based.UI
             if (sz is double s) FontSizeBox.SelectedItem = s;
         }
 
-        private void Editor_SelectionChanged(object sender, RoutedEventArgs e)
-        {
-
-        }
 
         private void InsertBtn(object sender, RoutedEventArgs e)
         {
-            Editor.AppendText(Clipboard.GetText());
+            if (Clipboard.ContainsText())
+            {
+                Editor.CaretPosition.InsertTextInRun(Clipboard.GetText());
+                string text = Clipboard.GetText();
+                if (Text == text)
+                {
+                    Clipboard.SetText("");
+                }
+            }
+            else
+                return;
         }
-
         private void CopyBtn(object sender, RoutedEventArgs e)
         {
-            Clipboard.SetText(Editor.Selection.Text);
+            try
+            {
+                Clipboard.SetText(Editor.Selection.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
-
         private void CutOutBtn(object sender, RoutedEventArgs e)
         {
             Clipboard.SetText(Editor.Selection.Text);
+            Text = Editor.Selection.Text;
             Editor.Selection.Text = "";
         }
+
+        private void RightBtn(object sender, RoutedEventArgs e)
+        {
+            Editor.Selection.ApplyPropertyValue(Paragraph.TextAlignmentProperty, TextAlignment.Right);
+        }
+        private void LeftBtn(object sender, RoutedEventArgs e)
+        {
+            Editor.Selection.ApplyPropertyValue(Paragraph.TextAlignmentProperty, TextAlignment.Left);
+        }
+        private void JustifyBtn(object sender, RoutedEventArgs e)
+        {
+            Editor.Selection.ApplyPropertyValue(Paragraph.TextAlignmentProperty, TextAlignment.Justify);
+        }
+        private void CenterBtn(object sender, RoutedEventArgs e)
+        {
+            Editor.Selection.ApplyPropertyValue(Paragraph.TextAlignmentProperty, TextAlignment.Center);
+
+        }
+
+
+        private void Bulleted_NumberingBtn(object sender, RoutedEventArgs e)
+        {
+            Number = 1;
+
+            Paragraph newParagraph = new Paragraph(new Run("\t● "));
+            Editor.Document.Blocks.Add(newParagraph);
+            Editor.CaretPosition = newParagraph.ContentEnd;
+            Editor.Focus();
+        }
+        private void Numerical_NumberingBtn(object sender, RoutedEventArgs e)
+        {
+            Paragraph newParagraph = new Paragraph(new Run($"\t{Number}. "));
+            Editor.Document.Blocks.Add(newParagraph);
+            Editor.CaretPosition = newParagraph.ContentEnd;
+            Editor.Focus();
+            Number++;
+        }
+
     }
 }
