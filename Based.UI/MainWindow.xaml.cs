69dca3b80caef3666e15868233c2d40a0510cf7e
zdkazuha <aartemchelyk@gmail.com>
2025-05-16 20:07:59 +0300

    Оновнив буфер та додав функціонал для абзац

--- Based.UI/MainWindow.xaml.cs
+++ Based.UI/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Numerics;
+using System.Reflection;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Based.UI
 {
@@ -16,35 +19,88 @@ namespace Based.UI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int Number = 1;
+        private bool IsNotNumbered = false;
+        private bool IsNumbered = false;
+
+        private string Text = "";
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
         private void Editor_SelectionChanged(object sender, RoutedEventArgs e)
         {
-
+            return;
         }
 
         private void InsertBtn(object sender, RoutedEventArgs e)
         {
-            Editor.AppendText(Clipboard.GetText());
+            if (Clipboard.ContainsText())
+            {
+                Editor.CaretPosition.InsertTextInRun(Clipboard.GetText());
+                string text = Clipboard.GetText();
+                if (Text == text)
+                {
+                    Clipboard.SetText("");
+                }
+            }
+            else
+                return;
         }
-
         private void CopyBtn(object sender, RoutedEventArgs e)
         {
-            Clipboard.SetText(Editor.Selection.Text);
+            try
+            {
+                Clipboard.SetText(Editor.Selection.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
-
         private void CutOutBtn(object sender, RoutedEventArgs e)
         {
             Clipboard.SetText(Editor.Selection.Text);
+            Text = Editor.Selection.Text;
             Editor.Selection.Text = "";
+        }
+
+        private void RightBtn(object sender, RoutedEventArgs e)
+        {
+            Editor.Selection.ApplyPropertyValue(Paragraph.TextAlignmentProperty, TextAlignment.Right);
+        }
+        private void LeftBtn(object sender, RoutedEventArgs e)
+        {
+            Editor.Selection.ApplyPropertyValue(Paragraph.TextAlignmentProperty, TextAlignment.Left);
+        }
+        private void JustifyBtn(object sender, RoutedEventArgs e)
+        {
+            Editor.Selection.ApplyPropertyValue(Paragraph.TextAlignmentProperty, TextAlignment.Justify);
+        }
+        private void CenterBtn(object sender, RoutedEventArgs e)
+        {
+            Editor.Selection.ApplyPropertyValue(Paragraph.TextAlignmentProperty, TextAlignment.Center);
+
+        }
+
+
+        private void Bulleted_NumberingBtn(object sender, RoutedEventArgs e)
+        {
+            Number = 1;
+
+            Paragraph newParagraph = new Paragraph(new Run("\t● "));
+            Editor.Document.Blocks.Add(newParagraph);
+            Editor.CaretPosition = newParagraph.ContentEnd;
+            Editor.Focus();
+        }
+        private void Numerical_NumberingBtn(object sender, RoutedEventArgs e)
+        {
+            Paragraph newParagraph = new Paragraph(new Run($"\t{Number}. "));
+            Editor.Document.Blocks.Add(newParagraph);
+            Editor.CaretPosition = newParagraph.ContentEnd;
+            Editor.Focus();
+            Number++;
         }
     }
 }
